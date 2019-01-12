1ed743c5f56cba63a8fa87a8e9605b2ce4db3382
Hyun Kim <bin.room@outlook.kr>
2019-01-12 19:00:43 +0900

    Added player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity) {
+        this.velocity = velocity;
+    }
+    public void LookAt(Vector3 point) {
+        Vector3 heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+    public void FixedUpdate() {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
