ac4513057895ad88ebe79ada60b00ce7acda6fc2
Hyun Kim <bin.room@outlook.kr>
2019-01-20 21:34:30 +0900

    Added Enemy.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+	public LayerMask collisionMask;
 	float speed = 10f;
 
 	public void SetSpeed (float _speed) {
 		speed = _speed;
 	}
-	// Update is called once per frame
+
 	void Update () {
-		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		float moveDistance = speed * Time.deltaTime;
+		CheckCollisions (moveDistance);
+		transform.Translate (Vector3.forward * moveDistance);
+	}
+
+	void CheckCollisions (float moveDistance) {
+		Ray ray = new Ray (transform.position,
+			transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, moveDistance, collisionMask,
+				QueryTriggerInteraction.Collide)) {
+			OnHitObject (hit);
+		}
+	}
+
+	void OnHitObject (RaycastHit hit) {
+		print (hit.collider.gameObject);
+		GameObject.Destroy (gameObject);
 	}
 }
